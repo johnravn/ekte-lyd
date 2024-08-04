23671730a4cfed7f53f1ef485c184a485bd9514b
John Ravndal <johnrav@ulrik.uio.no>
2024-08-04 15:59:22 +0200

    Added hero section on homepage

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,24 @@
-import { Button, Heading } from "@chakra-ui/react";
+import { Box, Button, Center, Image } from "@chakra-ui/react";
+import logoOnBlack from "../assets/logo/logoOnBlack.svg";
+import videoLoop from "../assets/videos/ektelyd_website_hero_loop_720p.mp4";
 
 export default function HeroSection() {
   return (
     <>
-      <Heading colorScheme="brand">HeroHeroHero</Heading>
+      <Center w={"100%"}>
+        <Image src={logoOnBlack} w={"360px"} alt="Ekte Lyd Logo"></Image>
+      </Center>
+      <Box
+        w={"1520px"}
+        h={{ base: "550px", lg: "450px" }}
+        overflow={"hidden"}
+        display={"block"}
+        // bgColor={"#aa2222"}
+      >
+        <video autoPlay loop muted id="videoLoop">
+          <source src={videoLoop} type="video/mp4" />
+        </video>
+      </Box>
       <Button colorScheme="brandPrimary">Send</Button>
       <Button colorScheme="brandSecondary">Send</Button>
     </>
