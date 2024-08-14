c4ad5a00e1f24b98c48638049a402281f8a7fea0
John Ravndal <johnrav@ulrik.uio.no>
2024-08-14 14:38:15 +0200

    Added video poster image on homescreen

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,7 @@
 import { Box, Center, Image } from "@chakra-ui/react";
 import logoOnBlack from "../assets/logo/logoOnBlack.svg";
 import videoLoop from "../assets/videos/ektelyd_website_hero_loop_720p.mp4";
+import poster from "../assets/images/videoPoster.jpeg";
 
 export default function HeroSection() {
   return (
@@ -15,7 +16,7 @@ export default function HeroSection() {
         display={"block"}
         // bgColor={"#aa2222"}
       >
-        <video autoPlay loop muted id="videoLoop">
+        <video autoPlay loop muted id="videoLoop" poster={poster}>
           <source src={videoLoop} type="video/mp4" />
         </video>
       </Box>
