0359157d4cf1371e368cc5775940abfc12da364d
John Ravndal <johnrav@ulrik.uio.no>
2024-09-03 15:39:05 +0200

    Small content change

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -20,7 +20,7 @@ export default function AboutUsPage() {
         <VStack spacing={"30px"}>
           <Paragraph
             title="Hvem er ekte lyd?"
-            text="Lidenskapen for lyd har vokst seg stor hos oss og er sterkt påvirket av musikalsk bakgrunn. Siden 2021 har vi jobbet som frilans lydteknikere i Oslo. Vi er gode kompiser i tillegg til kollegaer og  valgte derfor i 2023 å starte vårt eget firma sammen, Ekte Lyd."
+            text="Lidenskapen for lyd har vokst seg stor hos oss og er sterkt påvirket av musikalsk bakgrunn. Siden 2021 har vi jobbet som frilans lydteknikere i Oslo. Vi er gode kompiser i tillegg til kollegaer og  valgte derfor i 2023 å starte vårt eget firma sammen, ekte lyd."
           />
           <Paragraph text="Vi jobber for å levere produksjoner av høy kvalitet, samtidig som vi ønsker å være fleksible overfor kundens behov. Ikke minst ønsker vi å ha integritet, altså at vi er oppriktige folk, har ryddig kommunikasjon og at kundens suksess alltid er det viktigste." />
           <Paragraph text="Om du bare trenger å supplere med litt utstyr, eller om du ønsker prosjektering av teknisk produksjon til eventet ditt, er du hjertelig velkommen til å ta kontakt! En hyggelig prat og litt råd er alltid gratis! Gleder oss til å høre fra deg!" />
